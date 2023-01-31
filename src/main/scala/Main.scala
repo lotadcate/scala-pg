d44a1d2f3d24edf4c58a1bf828567fc3007649f0
lotadcate <lotadcate@gmail.com>
2023-01-31 12:00:56 +0900

    出力のフォーマットを統一

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -17,24 +17,14 @@ object MyModule {
     else fib(n-1) + fib(n-2)
   }
 
-  private def formatAbs(x: Int) = {
-    val msg = "The absolute value of %d is %d"
-    msg.format(x, abs(x))
-  }
-
-  private def formatFactorial(n: Int) = {
-    val msg = "The factorial of %d is %d"
-    msg.format(n, factorial(n))
-  }
-
-  private def formatFib(n: Int) = {
-    val msg = "The fib of %d is %d"
-    msg.format(n, fib(n))
+  private def formatResult(name: String, n: Int, f: Int => Int) = {
+    val msg = "The %s of %d is %d"
+    msg.format(name, n, f(n))
   }
 
   def main(args: Array[String]): Unit = {
-    println(formatAbs(-42))
-    println(formatFactorial(5))
-    println(formatFib(7))
+    println(formatResult("absolute value", -42, abs))
+    println(formatResult("factorial", 5, factorial))
+    println(formatResult("fib", 7, fib))
   }
 }
